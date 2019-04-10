d775cce4c3c8a2a6d91947501dec8cefb6c4842f
Mike M <mikal815@users.noreply.github.com>
2019-04-09 23:28:20 -0400

    Complete infinite slider w/images

--- src/components/featured/Carousel.js
+++ src/components/featured/Carousel.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Slider from "react-slick";
 
+import slide_one from '../../resources/images/slide_one.jpg'
+import slide_two from '../../resources/images/slide_two.jpg'
+import slide_three from '../../resources/images/slide_three.jpg'
+
 
 const Carousel = () => {
 
@@ -22,13 +26,37 @@ const Carousel = () => {
         >
             <Slider {...settings}>
                 <div>
-                    image 1
+                    <div
+                        className="carousel_image"
+                        style={{
+                            background: `url(${slide_one})`,
+                            height: `${window.innerHeight}px`
+                        }}
+                    >
+
+                    </div>
                 </div>
                 <div>
-                    image 2
+                    <div
+                        className="carousel_image"
+                        style={{
+                            background: `url(${slide_two})`,
+                            height: `${window.innerHeight}px`
+                        }}
+                    >
+
+                    </div>
                 </div>
                 <div>
-                    image 3
+                    <div
+                        className="carousel_image"
+                        style={{
+                            background: `url(${slide_three})`,
+                            height: `${window.innerHeight}px`
+                        }}
+                    >
+
+                    </div>
                 </div>
 
             </Slider>
