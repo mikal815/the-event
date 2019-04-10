7690f12c1e34d783b7cbc74a40ab4d003554e741
Mike M <mikal815@users.noreply.github.com>
2019-04-09 20:59:42 -0400

    Add menu icon/console.log onClick

--- src/components/header_footer/header.js
+++ src/components/header_footer/header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
@@ -17,7 +17,23 @@ class Header extends Component {
                         padding: '10px 0px'
                     }}
                 >
-                    header
+                    <Toolbar>
+
+                        <div className="header_logo">
+                            <div className="font_righteous header_logo_venue">Boardwalk Pier Stage</div>
+                            <div className="header_logo_title">Musical Events</div>
+                        </div>
+
+                        <IconButton
+                            aria-label="Menu"
+                            color="inherit"
+                            onClick={()=> console.log('open')}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+
+
+                    </Toolbar>
                 </AppBar>
             </div>
         );
