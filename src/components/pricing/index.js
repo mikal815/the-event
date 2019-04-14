9ea7d8aeb2849086474804973183626119664788
Mike M <mikal815@users.noreply.github.com>
2019-04-14 19:19:05 -0400

    Animate pricing boxes on pageload

--- src/components/pricing/index.js
+++ src/components/pricing/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import MyButton from '../utlis/MyButton';
 
+import Zoom from 'react-reveal/Zoom';
+
 class Pricing extends Component {
 
     state = {
@@ -11,12 +13,14 @@ class Pricing extends Component {
             'referrentur nam, eam ea discere facilisis. Ne sea sonet graeci comprehensam. Graece salutandi persecuti ad qui.',
             'Quo in habemus insolens, eu vidit referrentur nam, eam ea discere facilisis. Ne sea sonet graeci comprehensam.ece ndi cuti ad qui.' 
         ],
-        linkto:['http://sales/b','http://sales/f','http://sales/v']
+        linkto:['http://sales/b','http://sales/f','http://sales/v'],
+        delay:[500,0,500]
     }
 
     showBoxes = () => (
         this.state.prices.map((box,i)=>(
-            <div className="pricing_item">
+            <Zoom delay={this.state.delay[i]} key={i}>
+                <div className="pricing_item">
                 <div className="pricing_inner_wrapper">
                     <div className="pricing_title">
                         <span>${this.state.prices[i]}</span>
@@ -35,6 +39,8 @@ class Pricing extends Component {
                     </div>
                 </div>
             </div>
+            </Zoom>
+            
         ))
     )
 
