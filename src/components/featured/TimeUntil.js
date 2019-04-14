7d27014b2711ffe8102e12a8ff57af11547c1dd0
Mike M <mikal815@users.noreply.github.com>
2019-04-14 10:52:44 -0400

    Set state for TimeUntil component

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -4,7 +4,19 @@ import Slide from 'react-reveal/Slide';
 class TimeUntil extends Component {
 
     state = {
+        deadline: 'Dec, 16, 2019',
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        seconds:'0'
+    }
 
+    getTimeUntil(){
+
+    }
+
+    componentDidMount(){
+        setInterval(()=> this.getTimeUntil(this.state.deadline),1000)
     }
 
     render() {
