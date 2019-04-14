f5d5fa8e0e5f20634822a2fd43b5e3d5c77b5090
Mike M <mikal815@users.noreply.github.com>
2019-04-14 12:46:45 -0400

    Give zoom transition to item in Nfo section

--- src/components/venueNfo/index.js
+++ src/components/venueNfo/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Zoom from 'react-reveal/Zoom';
 
 import icon_calendar from '../../resources/images/icons/calendar.png';
 import icon_location from '../../resources/images/icons/location.png';
@@ -9,26 +10,30 @@ const VenueNfo = () => {
             <div className="center_wrapper">
                 <div className="vn_wrapper">
 
-                    <div className="vn_item">
-                        <div className="vn_outer">
-                            <div className="vn_inner">
-                                <div className="vn_icon_square bck_red"></div>
-                                <div 
-                                    className="vn_icon"
-                                    style={{
-                                        background:`url(${icon_calendar})`
-                                    }}
-                                ></div>
-                                <div className="vn_title">
-                                Event Date & Time
+                    <Zoom duration={500}>
+                        <div className="vn_item">
+                            <div className="vn_outer">
+                                <div className="vn_inner">
+                                    <div className="vn_icon_square bck_red"></div>
+                                    <div
+                                        className="vn_icon"
+                                        style={{
+                                            background: `url(${icon_calendar})`
+                                        }}
+                                    ></div>
+                                    <div className="vn_title">
+                                        Event Date & Time
                                 </div>
-                                <div className="vn_desc">
-                                    12 November 2019 @10.00 pm
+                                    <div className="vn_desc">
+                                        12 November 2019 @10.00 pm
                                 </div>
-                            </div>
+                                </div>
 
+                            </div>
                         </div>
-                    </div>
+
+                    </Zoom>
+                    
 
                     <div className="vn_item">
                         <div className="vn_outer">
