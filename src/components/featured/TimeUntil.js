9057b0560a832b645ba48c1d185a4db30aa2e072
Mike M <mikal815@users.noreply.github.com>
2019-04-14 11:13:01 -0400

    Calculate minutes, hours, and days until the event

--- src/components/featured/TimeUntil.js
+++ src/components/featured/TimeUntil.js
@@ -16,8 +16,11 @@ class TimeUntil extends Component {
         if(time < 0){
             console.log('Date passed')
         } else {
-            const seconds = Math.floor((time/1000)%60)
-            console.log(seconds)
+            const seconds = Math.floor((time/1000)%60);
+            const minutes = Math.floor((time/1000/60)%60);
+            const hours = Math.floor((time/(1000*60*60))%24);
+            const days = Math.floor(time/(1000*60*60*24))
+            
         }
         
     }
