5ce23e39ed47944906f92ebcfe8b5fdb78c213c8
Mike M <mikal815@users.noreply.github.com>
2019-04-14 17:43:52 -0400

    Add Pricing component

---
+++ src/components/pricing/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Pricing extends Component {
+    render() {
+        return (
+            <div className="bck_black">
+                <div className="center_wrapper pricing_section">
+                    <h2>Pricing</h2>
+
+                    <div className="pricing_wrapper">
+                        boxes
+                    </div>
+                    
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+export default Pricing;
