16d9548c67620bf426da5a160e6f5c96082c1aaa
Mike M <mikal815@users.noreply.github.com>
2019-04-14 14:00:26 -0400

    Add 'Highlights' component/Transition Fade h2

---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                Per dolorum detraxit expetendis ei, eam sint timeam an. Ei dolores apeirian pri. 
+                Quo in habemus insolens, eu vidit referrentur nam, eam ea discere facilisis. 
+                Ne sea sonet graeci comprehensam. Graece salutandi persecuti ad qui.
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
