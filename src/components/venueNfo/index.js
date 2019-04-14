e086c653ba1119576962907106ad301a5f6c6269
Mike M <mikal815@users.noreply.github.com>
2019-04-14 12:41:19 -0400

    Display event location icon and nfo on VenueNfo section

--- src/components/venueNfo/index.js
+++ src/components/venueNfo/index.js
@@ -18,17 +18,40 @@ const VenueNfo = () => {
                                     style={{
                                         background:`url(${icon_calendar})`
                                     }}
-                                >
-
+                                ></div>
+                                <div className="vn_title">
+                                Event Date & Time
+                                </div>
+                                <div className="vn_desc">
+                                    12 November 2019 @10.00 pm
                                 </div>
                             </div>
 
                         </div>
                     </div>
 
-                    <div>
+                    <div className="vn_item">
+                        <div className="vn_outer">
+                            <div className="vn_inner">
+                                <div className="vn_icon_square bck_yellow"></div>
+                                <div 
+                                    className="vn_icon"
+                                    style={{
+                                        background:`url(${icon_location})`
+                                    }}
+                                ></div>
+                                <div className="vn_title">
+                                Event Location
+                                </div>
+                                <div className="vn_desc">
+                                    521 5th street, Coastal, NJ 08223
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
+
+                    
 
                 </div>
 
