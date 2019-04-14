0f6d06df464127e1a04b09ad27e3d067fe1b42d4
Mike M <mikal815@users.noreply.github.com>
2019-04-14 16:58:01 -0400

    Animate percentage off div

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -3,13 +3,35 @@ import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 
 class Discount extends Component {
+
+    state = {
+        discountStart:0,
+        discountEnd:30
+    }
+
+    percentage = () => {
+        if(this.state.discountStart < this.state.discountEnd){
+            this.setState({
+                discountStart: this.state.discountStart + 1
+            })
+        }
+    }
+
+    componentDidUpdate(){
+        setTimeout(()=>{
+            this.percentage()
+        },30)
+    }
+
     render() {
         return (
             <div className="center_wrapper">
                 <div className="discount_wrapper">
-                    <Fade>
+                    <Fade
+                        onReveal={()=> this.percentage()}
+                    >
                         <div className="discount_percentage">
-                            <span>25%</span>
+                            <span>{this.state.discountStart}%</span>
                             <span>OFF</span>
                         </div>
                     </Fade>
